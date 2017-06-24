f85489cad96fc1c16cc078169327f0bd1a49bc09
William-N-Havard <william.havard@gmail.com>
2017-06-24 22:23:45 +0200

    Add files via upload

---
+++ speechcoco_API/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='speechcoco',
+   version='1.0',
+   author='William N. Havard',
+   author_email='william.havard@gmail.com',
+   packages=['speechcoco']
+   package_dir = {'speechcoco': 'speechcoco'},
+)
